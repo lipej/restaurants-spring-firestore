4aba5494fdf226fffba60af5228a4b2451c98446
Felipe José <lipejose@icloud.com>
2022-06-05 20:01:51 -0300

    feat: restaurant repository

--- src/main/java/dev/lipejose/restaurants/repositories/RestaurantRepository.java
+++ src/main/java/dev/lipejose/restaurants/repositories/RestaurantRepository.java
@@ -1,2 +1,7 @@
-package dev.lipejose.restaurants.repositories;public interface RestaurantRepository {
+package dev.lipejose.restaurants.repositories;
+
+import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
+import dev.lipejose.restaurants.models.Restaurant;
+
+public interface RestaurantRepository extends FirestoreReactiveRepository<Restaurant> {
 }
